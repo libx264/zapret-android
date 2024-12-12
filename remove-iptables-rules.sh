0a036197010d1bea08d12a27588f074a1b73cb7b
raspberry-spy <wisemisticaltree5@gmail.com>
2024-12-12 17:38:42 +0200

    .

--- remove-iptables-rules.sh
+++ remove-iptables-rules.sh
@@ -1,7 +1,5 @@
 iptables -t nat -D OUTPUT -p tcp --dport 53 -j DNAT --to-destination 8.8.8.8:53
 iptables -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 8.8.8.8:53
 
-iptables -t mangle -D POSTROUTING -p tcp --dport 443 -m mark ! --mark 0x40000000/0x40000000 -j NFQUEUE --queue-num 200 --queue-bypass
-iptables -t mangle -D POSTROUTING -p udp --dport 443 -m mark ! --mark 0x40000000/0x40000000 -j NFQUEUE --queue-num 200 --queue-bypass
-iptables -t mangle -D POSTROUTING -p tcp --dport 80 -m mark ! --mark 0x40000000/0x40000000 -j NFQUEUE --queue-num 200 --queue-bypass
-iptables -t mangle -D POSTROUTING -p udp --dport 80 -m mark ! --mark 0x40000000/0x40000000 -j NFQUEUE --queue-num 200 --queue-bypass
+iptables -t mangle -D POSTROUTING -p tcp  -j NFQUEUE --queue-num 200 --queue-bypass
+iptables -t mangle -D POSTROUTING -p udp  -j NFQUEUE --queue-num 200 --queue-bypass
