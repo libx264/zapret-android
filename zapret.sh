70f98a6382e40ca6fec96e40efc937130f2c61f1
libx264 <134723037+libx264@users.noreply.github.com>
2025-05-07 21:04:30 +0300

    Update zapret.sh

--- zapret.sh
+++ zapret.sh
@@ -1,8 +1,8 @@
-iptables -t nat -D OUTPUT -p tcp --dport 53 -j DNAT --to-destination 83.220.169.155:53
-iptables -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 83.220.169.155:53
+#iptables -t nat -D OUTPUT -p tcp --dport 53 -j DNAT --to-destination 83.220.169.155:53
+#iptables -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 83.220.169.155:53
 
-iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination 83.220.169.155:53
-iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination 83.220.169.155:53
+#iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination 83.220.169.155:53
+#iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination 83.220.169.155:53
 
 iptables -t mangle -D POSTROUTING -p tcp  -j NFQUEUE --queue-num 200 --queue-bypass
 iptables -t mangle -D POSTROUTING -p udp  -j NFQUEUE --queue-num 200 --queue-bypass
