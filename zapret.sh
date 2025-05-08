d426d0fc169035b348d7e956868796ba5b133ba2
libx264 <134723037+libx264@users.noreply.github.com>
2025-05-08 20:21:27 +0300

    Update zapret.sh

--- zapret.sh
+++ zapret.sh
@@ -15,4 +15,7 @@ chmod 655 ./*
 chmod +x .
 chmod +x ./*
 
-./nfqws --uid 1:3003 --qnum 200 --filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new --filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=quic_initial_www_google_com.bin --new --filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake
+./nfqws --uid 1:3003 --qnum 200 --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=8 --dpi-desync-fake-quic=quic_initial_www_google_com.bin --new \
+--filter-udp=50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new \
+--filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-autottl=3 --dpi-desync-fooling=md5sig --new \
+--filter-tcp=443 --dpi-desync=fake --dpi-desync-ttl=4 --dpi-desync-fake-tls-mod=rnd,rndsni,padencap
